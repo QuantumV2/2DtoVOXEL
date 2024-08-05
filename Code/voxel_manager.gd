5ed45e0cdd616ad5455d8df2b547e0371fd6a175
QuantumV3 <83087109+QuantumV2@users.noreply.github.com>
2024-08-05 22:25:12 +0300

    doen

--- Code/voxel_manager.gd
+++ Code/voxel_manager.gd
@@ -1,14 +1,16 @@
 extends Node3D
-
+const SIZE = 48
 var voxel_grid: Array = []
+var color_grid: Array = []
 var voxels_present = 0
+var meshinstance = null
 func init_voxel_grid(width, height, depth):
 	for z in range(depth):
 		var layer: Array = []
 		for y in range(height):
 			var row: Array = []
 			for x in range(width):
-				var tilepresent = true
+				var tilepresent = [false, [Color(1,1,1), Color(1,1,1), Color(1,1,1)]]
 				row.append(tilepresent)  # false means the voxel is empty
 				voxels_present += 1 if tilepresent else 0
 			layer.append(row)
@@ -38,27 +40,51 @@ func combine_voxel_meshes(_voxel_grid):
 	for z in range(_voxel_grid.size()):
 		for y in range(_voxel_grid[z].size()):
 			for x in range(_voxel_grid[z][y].size()):
-				if _voxel_grid[z][y][x]:
+				if _voxel_grid[z][y][x][0]:
 					for face in faces:
+						mesher.set_color(_voxel_grid[z][y][x][1][0])
 						mesher.add_vertex(vertices[face[0]] + Vector3(x, y, z))
 						mesher.add_vertex(vertices[face[1]] + Vector3(x, y, z))
+						#mesher.set_color(_voxel_grid[z][y][x][1][2])
 						mesher.add_vertex(vertices[face[2]] + Vector3(x, y, z))
 	mesher.generate_normals()
 	var combined_mesh = mesher.commit()
 	return combined_mesh
+	
+func update_voxels_from_sprite(img1,img2,img3):
 
+	for x in range(SIZE):
+		for y in range(SIZE):
+			for z in range(1, SIZE):
+				var pixel1 = img1.get_pixel(x, y)
+				var pixel2 = img2.get_pixel(SIZE - z, y)
+				var pixel3 = img3.get_pixel(x, SIZE - z)
+
+				if pixel1.a > 0.5 and pixel2.a > 0.5 and pixel3.a > 0.5:
+					voxel_grid[x][SIZE - 1 - y][z] = [true, [pixel1, pixel2, pixel3]]
+				else:
+					voxel_grid[x][SIZE - 1 - y][z] = [false, [Color(1,1,1), Color(1,1,1), Color(1,1,1)]]
 	
 func render_voxel_grid(_voxel_grid):
 	var combined_mesh = combine_voxel_meshes(_voxel_grid)
 	var mesh_instance = MeshInstance3D.new()
 	mesh_instance.mesh = combined_mesh
 	mesh_instance.cast_shadow = true
+	var mat = StandardMaterial3D.new()
+	mat.vertex_color_use_as_albedo = true
+	mesh_instance.material_override = mat
+	meshinstance = mesh_instance
 	add_child(mesh_instance)
-	print(voxels_present)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	voxel_grid = init_voxel_grid(64, 64, 64)
+	voxel_grid = init_voxel_grid(SIZE, SIZE, SIZE)
+	# load sprites
+	var sprite1 = Image.load_from_file("res://Sprites/sfront.png")  # front view
+	var sprite2 = Image.load_from_file("res://Sprites/sside.png")  # side view
+	var sprite3 = Image.load_from_file("res://Sprites/top.png")  # top view
+
+	update_voxels_from_sprite(sprite1, sprite2, sprite3)
 	render_voxel_grid(voxel_grid)
 	pass # Replace with function body.
 
